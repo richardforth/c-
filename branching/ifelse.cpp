0968fd8afc7015deb2e2dfbbfb892d431ba9dfd6
Richard Forth <richard.forth@gmail.com>
2022-11-10 07:15:44 +0000

    Create ifelse.cpp

---
+++ branching/ifelse.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 8;
+    char letter = 'A';
+
+    if ( num > 5 )
+    { cout << "Number exceeds five" << endl; }
+    else
+    { cout << "Number is five or less" << endl; }
+
+    return 0;
+}
