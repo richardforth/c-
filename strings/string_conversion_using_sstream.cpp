952f0300913471664985c150ec7431bc219bb117
Richard Forth <richard.forth@gmail.com>
2022-11-11 12:42:11 +0000

    Create string_conversion_using_sstream.cpp

---
+++ strings/string_conversion_using_sstream.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string term = "100" ;
+    int number= 100 ;
+    int num ;
+    string text ;
+    stringstream stream ;
+
+    stream << term ;
+    stream >> num ;
+
+    num /= 4;
+    cout << "Integer value: " << num << endl;
+
+    // reset the stringstream ready for re-use
+    stream.str("") ; // empty he contents
+    stream.clear() ; // empty the bit flags
+
+    stream << number ;
+    stream >> text ;
+    text += " Per Cent" ;
+    cout << "String value: " << text << endl ;
+
+    return 0;
+}
