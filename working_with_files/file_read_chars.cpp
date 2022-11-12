42cc8708a9282d2b17f167c573a4aed86dc61917
Richard Forth <richard.forth@gmail.com>
2022-11-12 15:57:16 +0000

    Create file_read_chars.cpp

---
+++ working_with_files/file_read_chars.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+    char letter ;
+    int i ;
+    ifstream reader( "poem.txt" ) ;
+
+    if ( ! reader )
+    {
+        cout << "Error, unable to open file for reading." ;
+        return -1 ;
+    }
+    else
+    {
+        for ( i = 0; ! reader.eof(); i++)
+        {
+            reader.get( letter ) ;
+            cout << letter ;
+
+        }
+        reader.close() ;
+        cout << endl << "Number of iterations: " << i << endl ;
+    }
+
+    return 0;
+}
