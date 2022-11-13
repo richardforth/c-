28154464e1c2e2a353ad4ff9ae996b5b2b7dbb2b
Richard Forth <richard.forth@gmail.com>
2022-11-13 08:20:56 +0000

    Create handle_exceptions1.cpp

---
+++ exceptions/handle_exceptions1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string lang = "C++" ;
+    int num = 1000000000 ;
+    try
+    {
+        lang.replace( 100, 1, "C") ;
+    }
+    catch ( out_of_range &e )
+    {
+        cerr << "Range Exception: " << e.what() << endl ;
+        cerr << "Exception Type: " << typeid( e ).name() << endl ;
+        cerr << "Program terminated." << endl ;
+        return -1 ;
+    }
+
+    cout << "Program continues..." << endl ;
+    return 0 ;
+}
