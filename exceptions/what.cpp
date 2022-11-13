00ee9b61fa05c1d012304819ff58f08064416494
Richard Forth <richard.forth@gmail.com>
2022-11-13 08:09:26 +0000

    Create what.cpp

---
+++ exceptions/what.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string lang = "C++" ;
+
+    try
+    {
+        lang.erase( 4, 6 ) ;
+    }
+    catch ( exception &error )
+    {
+        cerr << "Exception: " << error.what() << endl ;
+    }
+
+
+    return 0 ;
+}
