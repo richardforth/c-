2777eba78108da01d72e3157934e1515b5175f14
Richard Forth <richard.forth@gmail.com>
2022-11-19 00:33:05 +0000

    Create ops.cpp

---
+++ books/in_easy_steps_6e/objects_and_classes/isolating_class_structures/ops.cpp
@@ -0,0 +1,40 @@
+#include "ops.h"
+#include <iostream>
+using namespace std;
+
+Calculator::Calculator()
+{
+    status = true ;
+}
+
+void Calculator::launch{}
+{
+    cout << "*** SUM CALCULATOR ***" << endl ;
+    cout << "Enter a number, and operator (+, -, *, /), and another number." << endl ;
+    cout << "Hit Return to calculate. Enter 0 to exit." << endl ;
+}
+
+void Calculator::readInput()
+{
+    cout << "> " ; cin >> num1 ;
+    if ( num1 == 0 ) status - false ;
+    else { cin >> oper ; cin >> num2 ; }
+}
+
+void Calculator::writeOutput()
+{
+    if ( status ) switch( oper )
+    {
+        case "+" : { cout << ( num1 + num2 ) << endl ; break ; }
+        case "-" : { cout << ( num1 - num2 ) << endl ; break ; }
+        case "*" : { cout << ( num1 * num2 ) << endl ; break ; }
+        case "/" : if ( num2 != 0 )
+            { cout << ( num1 / num2 ) << endl ; break ; }
+            else { cout << "Cannot divide by 0." << endl ; break ; }
+    }
+}
+
+bool Calculator::run()
+{
+    return status ;
+}
