c671493e3065be38aa0f208283320215b50b91a4
Richard A. Forth <richard.forth@gmail.com>
2022-11-20 15:12:19 +0000

    Added scripts

---
+++ books/in_easy_steps_6e/macros/define.cpp
@@ -0,0 +1,13 @@
+#define BOOK "C++ Programming in easy steps"
+#define NUM 200
+#define RULE "*****************************"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << RULE << endl << BOOK << endl << RULE ;
+	cout << endl << "NUM is: " << NUM << endl ;
+	cout << "Double NUM: " << ((NUM) * 2 ) << endl ;
+	return 0;
+}
