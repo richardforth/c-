d23a26ecd1a8c2261ee89633563853b865bf2bea
Richard Forth <richard.forth@gmail.com>
2022-11-23 20:00:27 +0000

    Create do_while_loop.cpp

---
+++ courses/cpp_android_app_by_programming_hub/do_while_loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // local variable declaration
+    int a = 10;
+
+    // while loop execution
+    do{
+        cout<<"value of a: "<<a<<endl;
+        a++;
+    }while(a<20) ;
+
+    return 0;
+}
