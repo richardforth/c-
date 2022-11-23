442fe5db8af1ea5f3aaa71f77fea3e4a0d93b386
Richard Forth <richard.forth@gmail.com>
2022-11-23 19:32:57 +0000

    Create vote_test.cpp

---
+++ courses/cpp_android_app_by_programming_hub/vote_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 15; 
+    if (age >= 18){      
+        cout<< "You are eligible to vote";
+    } 
+    else{     
+        cout<< "Nope, you cannot vote";
+    }
+
+    return 0;
+}
