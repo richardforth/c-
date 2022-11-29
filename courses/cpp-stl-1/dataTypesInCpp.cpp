ece3e7fe4441b3bd771b49a452480972a2101d7a
Richard Forth <richard.forth@gmail.com>
2022-11-29 19:19:21 +0000

    Create dataTypesInCpp.cpp

---
+++ courses/cpp-stl-1/dataTypesInCpp.cpp
@@ -0,0 +1,19 @@
+/* not an exhaustive list */
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 39;
+    string name = "Sandeep";
+    char gender = 'M';
+    bool isMarried = 1;
+    float weight = 68.5;
+    cout << name << endl
+         << age << endl
+         << gender << endl
+         <<isMarried << endl
+         << weight << endl;
+
+    return 0;
+}
