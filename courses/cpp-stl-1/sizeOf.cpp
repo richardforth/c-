20f0d5b92139e8f2e11a9118a3afe5fbe326b3ee
Richard Forth <richard.forth@gmail.com>
2022-11-30 20:24:03 +0000

    Update sizeOf.cpp

--- courses/cpp-stl-1/sizeOf.cpp
+++ courses/cpp-stl-1/sizeOf.cpp
@@ -8,5 +8,15 @@ int main()
     cout << sizeof(long long) << endl;
     cout << sizeof(float) << endl;
     cout << sizeof(double) << endl;
+
+
+    int x;
+    double d;
+    cout << sizeof(x) << endl;
+    x = 10;
+    cout << sizeof(x) << endl;
+    cout << sizeof(d) << endl;
+    cout << sizeof(10ll) << endl;
+    cout << sizeof(10.5) << endl;
     return 0;
 }
