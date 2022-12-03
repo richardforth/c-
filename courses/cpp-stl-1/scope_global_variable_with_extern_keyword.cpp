2cccc10b401a593d5f9dd57e3434320b9a953588
Richard Forth <richard.forth@gmail.com>
2022-12-03 22:47:58 +0000

    Create scope_global_variable_with_extern_keyword.cpp

---
+++ courses/cpp-stl-1/scope_global_variable_with_extern_keyword.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#using namespace std;
+
+// variable declaration only with extern keyword to prevent compiler error
+
+
+int main()
+{
+  cout << x << endl;
+  return 0;
+}
+
+// global variable declared below the function
+int x = 10;
